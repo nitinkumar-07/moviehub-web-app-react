e63a8e772ce7b02a3dc74d22dc2c0375be093d0e
nitin kumar <somukumar554@gmail.com>
2025-12-19 18:59:13 +0530

    fix page refresh

--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -11,16 +11,14 @@ import { Route, Routes } from "react-router-dom"
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route>
-        <Route path="/" element = {<Trending/>}/>
-        <Route path="/movies" element = {<MovieList/>}/>
-        <Route path="/tvshows" element = {<TvShowList/>}/>
-        <Route path="/search/:searchName/" element = {<SearchList />} />
-        <Route path="/genres/movies" element={<MovieList />} />
-        <Route path="/player/:playerId" element={<MoviePlayer/>}/>
-        <Route path="/playerr/:playerId" element={<TvShowPlayer/>}/>
-      </Route>
-     </Routes>
+      <Route path="/" element={<Trending />} />
+      <Route path="/movies" element={<MovieList />} />
+      <Route path="/tvshows" element={<TvShowList />} />
+      <Route path="/search/:searchName/" element={<SearchList />} />
+      <Route path="/genres/movies" element={<MovieList />} />
+      <Route path="/player/:playerId" element={<MoviePlayer />} />
+      <Route path="/playerr/:playerId" element={<TvShowPlayer />} />
+    </Routes>
   )
 }
 
