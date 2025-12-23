372fcb88edbf4b15a94134e196d30b33e0811c34
nitin kumar <somukumar554@gmail.com>
2025-12-23 08:54:54 +0530

    fix refreshes

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -2,18 +2,18 @@ import useMovieList from "@/hooks/useMovies"
 import MovieCard from "./MovieCard";
 import { useContext } from "react";
 import { GenresContext } from "@/context/genres.context";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
 const MovieList = () => {
 
-    const { genres, genreName } = useContext(GenresContext);
+    const { genres, genreName, setGenres, setGenreName } = useContext(GenresContext);
     const { movieLists } = useMovieList(genres);
     //console.log(movieLists)
 
-    // useEffect(() => {
-    //     setGenres(null);
-    //     setGenreName(null);
-    // }, []);
+    useEffect(() => {
+        setGenres(null);
+        setGenreName(null);
+    }, []);
 
     return <div className="w-full max-w-[1450px] p-2 mx-auto">
         <h1 className="text-2xl font-semibold p-3 py-2 text-[#cfc7f5]"> {genreName ? genreName : "Movies"}</h1>
