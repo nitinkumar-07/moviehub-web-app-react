7181940bb797e6db49afb47ce667c867b524f28c
nitin kumar <somukumar554@gmail.com>
2026-01-08 09:02:31 +0530

    add scrooltotop

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { GenresProvider } from './context/genres.context.tsx'
 import { SearchProvider } from './context/searchResult.context.tsx';
 import { ThemeProvider } from "./context/theme.context";
+import ScrollToTop from 'react-scroll-to-top'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
         <GenresProvider>
           <ThemeProvider>
             <App />
+            <ScrollToTop smooth color='white' style={{ backgroundColor: '#1f1e55', display: 'flex', alignItems: 'center', justifyContent: 'center', }} />
           </ThemeProvider>
         </GenresProvider>
       </SearchProvider>
